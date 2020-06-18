7fd824a1060747904e8afb2603e55e934e24ca1b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-18 07:29:09 +0000

    Deploy haggstrom/trivia-quiz to github.com/haggstrom/trivia-quiz.git:gh-pages

---
+++ precache-manifest.9e26425164afb18131b6b64a64fe1f77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b1024b4dd49ee028352ceff868dbef",
+    "url": "/trivia-quiz/index.html"
+  },
+  {
+    "revision": "4b7c4459b692a6980451",
+    "url": "/trivia-quiz/static/css/main.fe5a9cb3.chunk.css"
+  },
+  {
+    "revision": "891e9ea5c20f124784eb",
+    "url": "/trivia-quiz/static/js/2.9dbdf970.chunk.js"
+  },
+  {
+    "revision": "32d9c3460ee62d17fcc60d35208e458b",
+    "url": "/trivia-quiz/static/js/2.9dbdf970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b7c4459b692a6980451",
+    "url": "/trivia-quiz/static/js/main.05a2b62f.chunk.js"
+  },
+  {
+    "revision": "5d12c1007cdf69cd52d1",
+    "url": "/trivia-quiz/static/js/runtime-main.fa0a8b03.js"
+  }
+]);
